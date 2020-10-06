b22b1371b525c27d0fc68dde323b2bfaec05af30
Kukkui <kukkui2537@gmail.com>
2020-10-07 03:13:19 +0700

    make it better10

--- index.js
+++ index.js
@@ -7,30 +7,30 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/login'))
-  .post('/auth',function(request,response){
-    var username = request.body.username;
-    var password = request.body.password;
-    if (username && password) {
-        response.send("Good");
-        // connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+  .post('/auth',(req, res) => res.render('pages/index'))
+  //   var username = request.body.username;
+  //   var password = request.body.password;
+  //   if (username && password) {
+  //       response.send("Good");
+  //       // connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
         
-        // if(results.length > 0){
-        //     //set session here
-        //     //session name at the end request.session.xxx ... xxx is session name
-        //     request.session.loggedin = true;
-        //     request.session.username = username;
-        //     response.redirect('/home');
-        }else{
-            response.send('Incorrect account');
-        }
+  //       // if(results.length > 0){
+  //       //     //set session here
+  //       //     //session name at the end request.session.xxx ... xxx is session name
+  //       //     request.session.loggedin = true;
+  //       //     request.session.username = username;
+  //       //     response.redirect('/home');
+  //       }else{
+  //           response.send('Incorrect account');
+  //       }
 
-        // response.end();
-        });
-    }
-    else{
-        response.send('Please enter Username and Passeord');
-        // response.end();
-    }
+  //       // response.end();
+  //       });
+  //   }
+  //   else{
+  //       response.send('Please enter Username and Passeord');
+  //       // response.end();
+  //   }
   
-  })
+  // })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
