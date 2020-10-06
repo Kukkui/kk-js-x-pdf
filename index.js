8579786835c62e7645ebbd66bc42220ded14a144
Kukkui <kukkui2537@gmail.com>
2020-10-07 01:23:08 +0700

    make it better3

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/login'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   app.use(express.static(__dirname));
